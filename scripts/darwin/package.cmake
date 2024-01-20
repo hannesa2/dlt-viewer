29a51bc1cbd6b691d981947c46ef81688f942f0e
Hannes Achleitner <hannes.software@gmx.at>
2024-01-20 08:06:54 +0100

    Verbose

--- scripts/darwin/package.cmake
+++ scripts/darwin/package.cmake
@@ -4,9 +4,17 @@ endif()
 
 # See build.sh and src/cmake/Darwin.cmake
 set(CPACK_GENERATOR External)
+message(STATUS "MACDEPLOYQT_EXECUTABLE package-cmake.pre ${MACDEPLOYQT_EXECUTABLE}")
+message(STATUS "CMAKE_SYSTEM_PROCESSOR:       ${CMAKE_SYSTEM_PROCESSOR}")
 
-get_target_property(MOC_LOCATION ${QT_PREFIX}::moc LOCATION)
-get_filename_component(MACDEPLOYQT_EXECUTABLE ${MOC_LOCATION}/../macdeployqt ABSOLUTE)
+if(NOT "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "arm64")
+    get_target_property(MOC_LOCATION ${QT_PREFIX}::moc LOCATION)
+    get_filename_component(MACDEPLOYQT_EXECUTABLE ${MOC_LOCATION}/../macdeployqt ABSOLUTE)
+    message(STATUS "MACDEPLOYQT_EXECUTABLE x86 package-cmake ${MACDEPLOYQT_EXECUTABLE}")
+else()
+    message(STATUS "MACDEPLOYQT_EXECUTABLE arm64 package-cmake ${MACDEPLOYQT_EXECUTABLE}")
+endif()
+
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/scripts/darwin/macdeployqt.cmake.in" "${CMAKE_BINARY_DIR}/macdeployqt.cmake" @ONLY)
 
 set(CPACK_EXTERNAL_PACKAGE_SCRIPT "${CMAKE_BINARY_DIR}/macdeployqt.cmake")
